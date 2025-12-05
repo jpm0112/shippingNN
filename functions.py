45eb35bcdfa7f664b45b6e35211684444212ef42
jpm0112 <105248447+jpm0112@users.noreply.github.com>
2025-12-04 23:55:36 -0300

    Add clean_gpu function to manage GPU memory

--- functions.py
+++ functions.py
@@ -1726,3 +1726,11 @@ def run_darts_tft_with_for(df,
     mean_epochs = np.mean(n_epochs_values)  # dummy values for errors
     return (np.mean(mae_values), np.mean(mape_values), np.mean(mse_values),
             np.mean(rmse_values), np.mean(r2_values), mean_epochs, np.std(mape_values))
+
+
+
+def clean_gpu():
+    gc.collect()
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
