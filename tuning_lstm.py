da4b874d2d3dc3d07a2f09d67041c0b68d216b93
JP <jpm0112@auburn.edu>
2025-12-14 22:31:32 -0300

    udaptes runs

--- tuning_lstm.py
+++ tuning_lstm.py
@@ -20,7 +20,7 @@ from functions import (
 #  LOAD DATA
 # ============================================================
 initial_test_size = 12
-iterations = 50
+iterations = 100
 
 df = pd.read_csv("weekly_chile_data.csv")
 df["FECHA"] = pd.to_datetime(df["FECHA"])
