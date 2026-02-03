536be9f6af962dff4154decb3c22da95a8537378
Konstantinos Ziliaskopoulos <zilikons93@gmail.com>
2026-02-03 15:05:51 -0600

    changes the seed

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Project dev environment (Python + uv + Julia)";
+  description = "Project dev environment (Python + uv)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
