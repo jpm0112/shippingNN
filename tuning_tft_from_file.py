ed4320efbcca96e5803160918013784f7d953524
jpm0112 <jpm0112@auburn.edu>
2026-02-13 23:40:37 -0600

    udpate

--- tuning_tft_from_file.py
+++ tuning_tft_from_file.py
@@ -256,8 +256,12 @@ if __name__ == "__main__":
         raise RuntimeError("No GPUs detected.")
 
     jobs = [
-        ("NAW", 12),
-        ("FE", 12),
+        ("SAE", 12),
+        ("SAW", 12),
+        ("NAE", 12),
+        ("NE", 12),
+        ("NE", 4),
+        ("SE", 12),
     ]
 
     splits = np.array_split(jobs, available_gpus)
